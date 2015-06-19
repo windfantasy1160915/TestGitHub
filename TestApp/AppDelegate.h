d67ebf468738d31623393c6f39e83e3e51477948
windfantasy <734371367@qq.com>
2015-06-19 17:10:50 +0800

    我创建了工程

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by lanou3g on 15/6/19.
+//  Copyright (c) 2015年 李坦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by lanou3g on 15/6/19.
+//  Copyright (c) 2015年 李坦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
